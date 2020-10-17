a82b4c75de45c15565f23699a7431ae112e98d97
tommaay <tommy.huynh.dev@gmail.com>
2020-10-17 09:56:17 -0700

    Annotations and Inference basics

---
+++ annotations/variables.ts
@@ -0,0 +1,18 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let now: Date = new Date();
+
+let colors: string[] = ['red', 'green', 'blue'];
+
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+const day = new Date();
+
+let numAboveZero: number | boolean = false;
